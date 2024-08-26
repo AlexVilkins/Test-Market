f4c77eefacd0d45bd739c172568e91c2a6b177ca
Tomykuk <hetagpuhaev@mail.ru>
2024-08-26 20:31:17 +0300

    add  redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
 import App from "./App";
-import { store } from "./redux/store";
+// import { store } from "./redux/store";
 import "./scss/app.scss";
 
 const root = ReactDOM.createRoot(
@@ -13,9 +15,11 @@ const root = ReactDOM.createRoot(
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
   </Provider>
   // </React.StrictMode>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import products from "./products/slice";
 import basket from "./basket/slice";
 import favorite from "./favorite/slice";
 import order from "./order/slice";
 
-export const store = configureStore({
-  reducer: { products, basket, favorite, order },
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["products"],
+};
+
+const rootReducer = combineReducers({
+  products,
+  basket,
+  favorite,
+  order,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
